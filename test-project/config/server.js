ea19877f4e246cc443ccd0f3a41a66243feba2db
Josh Green <josh.green@wearecube3.co.uk>
2021-10-01 09:06:01 +0100

    first commit

---
+++ test-project/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a0f0789c73845304c0a64faacb930eb7'),
+    },
+  },
+});
---
+++ test-project/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8277ea41-948e-45c6-b41d-f4a59ea9212b'
+};
